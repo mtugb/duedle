66faf9562eb449eddba08ad6dd4c92fc01075c75
soushi53 <soushi888@icloud.com>
2026-03-18 00:56:08 +0900

    データ取得 やりはじめ

---
+++ src/assignment/dashboard.js
@@ -0,0 +1,30 @@
+
+const filterTypes = [
+    {
+        name: "completed",
+        displayName: "完了",
+    },
+    {
+        name: "incomplete",
+        displayName: "未完了",
+    }
+];
+
+const boxSelect = document.getElementsByClassName("calendarwrapper");
+const form = document.createElement("form");
+form.setAttribute("id", "filterForm");
+boxSelect[0].appendChild(form);
+
+for (let i = 0; i < filterTypes.length; i++) {
+    const filter = document.createElement("input");
+    filter.setAttribute("type", "checkbox");
+    filter.setAttribute("name", "assignmentProperty");
+    filter.setAttribute("value", filterTypes[i].name);
+    filter.classList.add("filter-input");
+    boxSelect[0].insertBefore(filter, boxSelect[0].firstChild);
+
+    const label = document.createElement("label");
+    label.setAttribute("for", filterTypes[i].name);
+    label.textContent = filterTypes[i].displayName;
+    boxSelect[0].insertBefore(label, boxSelect[0].firstChild);
+}
