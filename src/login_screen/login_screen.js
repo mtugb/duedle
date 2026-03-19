09b2bceee32971193e93ce6e40a5b9a8ad8f37f0
RyosukeOkazaki-0408 <kamome0423@gmail.com>
2026-03-20 03:10:59 +0900

    バグ修正

--- src/login_screen/login_screen.js
+++ src/login_screen/login_screen.js
@@ -1,6 +1,12 @@
 //余計な文章の変更
 const message = document.querySelector(".box.py-3.generalbox p");
-if (message) {
-    message.textContent = "2023年度より、本サーバをMoodleサーバとして運用しています。";
+if (message && message.textContent.includes("2023年度より、本サーバを Moodle サーバとして運用します。")) {
+    message.textContent = "2023年度より、本サーバを Moodle サーバとして運用しています。";
     message.style.fontWeight = "bold";
 }
+
+const login = document.querySelector("span.login.nav-link");
+if (login && login.textContent.includes("あなたはログインしていません。")) {
+    login.textContent = login.textContent.replace("。", "");
+}
+
