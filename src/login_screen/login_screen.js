a77293fd3da36d1efbebbf363c0c481572a318fc
RyosukeOkazaki-0408 <kamome0423@gmail.com>
2026-03-20 02:46:58 +0900

    微々たる修正

--- src/login_screen/login_screen.js
+++ src/login_screen/login_screen.js
@@ -1,6 +1,6 @@
 //余計な文章の変更
 const message = document.querySelector(".box.py-3.generalbox p");
 if (message) {
-    message.textContent = "2023年度より、本サーバをMoodleサーバとして運用します。";
+    message.textContent = "2023年度より、本サーバをMoodleサーバとして運用しています。";
     message.style.fontWeight = "bold";
 }
