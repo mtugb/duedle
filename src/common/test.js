2a8ddfc6e780d86c042c17a3d00266463f565028
ketomx <xketom611@gmail.com>
2026-03-19 23:04:44 +0900

    test

--- src/common/test.js
+++ src/common/test.js
@@ -1,1 +1,7 @@
-window.alert('ようこそDuedleへ');
+const li = document.createElement("li");
+const parentList = document.querySelector('ul[aria-labelledby = "sortingdropdown"]');
+li.innerHTML ='<li><a class="dropdown-item" href="#" data-filter="sort" data-pref="lastaccessed" data-value="ul.displayorder asc" role="menuitem">任意の順番に並べ替える</a></li>';
+
+if (parentList)
+    parentList.appendChild(li);
+
