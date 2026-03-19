b30f1f991ac4936b1c0cb934b318a14dbd49d4aa
soushi53 <soushi-sakurasoushi@outlook.jp>
2026-03-19 16:37:19 +0900

    no

--- src/assignment/dashboard.js
+++ src/assignment/dashboard.js
@@ -1,9 +1,9 @@
 
-const filterTypes = [
+const filterTypes_status = [
+
     {
         name: "completed",
         displayName: "完了",
-        group: "",
     },
     {
         name: "incomplete",
@@ -19,13 +19,16 @@ boxSelect[0].appendChild(form);
 for (let i = 0; i < filterTypes.length; i++) {
     const filter = document.createElement("input");
     filter.setAttribute("type", "checkbox");
-    filter.setAttribute("name", "assignmentProperty");
+    filter.setAttribute("name", filterTypes[i].name);
+    filter.setAttribute("id", filterTypes[i].name);
     filter.setAttribute("value", filterTypes[i].name);
     filter.classList.add("filter-input");
     boxSelect[0].insertBefore(filter, boxSelect[0].firstChild);
 
     const label = document.createElement("label");
     label.setAttribute("for", filterTypes[i].name);
-    label.textContent = filterTypes[i].displayName;
+    label.textContent = "　"+filterTypes[i].displayName;
     boxSelect[0].insertBefore(label, boxSelect[0].firstChild);
 }
+
+//checkbox event
