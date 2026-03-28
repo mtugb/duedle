5af8050c7cd408628b981a8b39a38f4f5885156a
mtugb laptop <main@mtugb.com>
2026-03-28 13:19:54 +0900

    fix:darkmode reactivated on dyllabus page

--- src/syllabus/darkmode.js
+++ src/syllabus/darkmode.js
@@ -1,4 +1,4 @@
-(() => {
+const darkmode_setup = () => {
   const colored_td = document.querySelectorAll('td[bgcolor="lightsteelblue"]');
   colored_td.forEach((e) => {
     e.setAttribute("bgcolor", "black");
@@ -11,4 +11,23 @@
       .replaceAll("lightsteelblue", "black");
     e.setAttribute("style", new_style);
   });
+};
+
+(async () => {
+  //0:sync
+  //1:on, 2:off
+  let darkmode = (await chrome.storage.local.get("darkmode")).darkmode ?? 0;
+  let shouldBeDarkmode =
+    (darkmode == 0 && isBrowserDarkMode()) || darkmode == 1;
+  if (shouldBeDarkmode) {
+    document.body.classList.add("darkmode");
+    darkmode_setup();
+  } else {
+    try {
+      document.body.classList.remove("darkmode");
+    } catch (_) {
+      //エラー無視
+    }
+  }
+  // chrome.storage.local.set({ history });
 })();
