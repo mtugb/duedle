7649b5ef26e109d37547d37d81e9f3c843a1cc4b
soushi53 <soushi888@icloud.com>
2026-04-05 16:19:08 +0900

    修正

--- src/storage/display/course.js
+++ src/storage/display/course.js
@@ -166,7 +166,7 @@ const transDue = (txt) => {
             Number(minute)
         );
     } else {
-        const time = txt.match(/(\d{4})年\s*(\d{2})月\s*(\d{2})日,\s*(\d{2}):(\d{2})/);
+        const time = txt.match(/(\d{4})年\s*(\d{1,2})月\s*(\d{1,2})日,\s*(\d{1,2}):(\d{2})/);
         if (time) {
             const [, year, month, day, hour, minute] = time;
             return new Date(
