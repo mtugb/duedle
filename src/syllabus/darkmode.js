a80a5a24b61e003e85cf577fa5fac47da47bf1c9
soushi53 <soushi-sakurasoushi@outlook.jp>
2026-04-27 14:18:30 +0900

    darkmdoe fix

--- src/syllabus/darkmode.js
+++ src/syllabus/darkmode.js
@@ -16,15 +16,23 @@ const darkmode_setup = () => {
 (async () => {
   //0:sync
   //1:on, 2:off
-  let darkmode = (await chrome.storage.local.get("darkmode")).darkmode ?? 0;
+  let thememode = (await chrome.storage.local.get("thememode")).thememode ?? 0;
   let shouldBeDarkmode =
-    (darkmode == 0 && isBrowserDarkMode()) || darkmode == 1;
+    (thememode == 0 && isBrowserDarkMode()) || thememode == 1;
   if (shouldBeDarkmode) {
     document.body.classList.add("darkmode");
     darkmode_setup();
+  } else if (thememode == 3) {
+    try {
+      document.body.classList.remove("darkmode");
+      document.body.classList.add("honobono");
+    } catch (_) {
+      //エラー無視
+    }
   } else {
     try {
       document.body.classList.remove("darkmode");
+      document.body.classList.remove("honobono");
     } catch (_) {
       //エラー無視
     }
